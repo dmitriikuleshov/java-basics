fa1786d7c23b164ee2b5d6f682e31ab866bccb09
dmitriikuleshov <dmkuleshov10@gmail.com>
2025-09-17 00:57:53 +0300

    added ReadBytes.java

--- src/main/java/com/dmitriikuleshov/javabasics/io/ReadBytes.java
+++ src/main/java/com/dmitriikuleshov/javabasics/io/ReadBytes.java
@@ -1,5 +1,19 @@
+// Read an array of bytes from the keyboard
+
 package com.dmitriikuleshov.javabasics.io;
 
-public class ReadBytes {
+import java.io.IOException;
 
+
+public class ReadBytes {
+    public static void main(String[] args) throws IOException {
+        byte[] data = new byte[10];
+
+        System.out.println("Enter some characters: ");
+        System.in.read(data);
+        System.out.print("You entered: ");
+        for (byte datum : data) {
+            System.out.print((char) datum);
+        }
+    }
 }
